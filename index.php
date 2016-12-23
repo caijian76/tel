7fefa2c9d6f6f4426ccaf47ff9825571bed7969b
蔡坚 <caijian@caijiandeMacBook-Pro.local>
2016-12-24 01:29:46 +0800

    add * file

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test page</title>
+
+    <script>
+        var test;
+        var test2;
+        console.log("hello world!");
+        console.log("hello world3!");
+        alert("hello");
+    </script>
+</head>
+<body>
+<h1>hi</h1>
+<a href="tt.html">tt</a>
+
+
+    <?php
+class fir
+{
+    var $a;
+    function __construct()
+    {
+       $this->a='蔡坚';
+    }
+
+
+    function ret()
+    {
+        return $this->a;
+    }
+}
+
+$b=new fir();
+$b->a='ooo';
+echo $b->ret();
+?>
+</body>
+
+
+
